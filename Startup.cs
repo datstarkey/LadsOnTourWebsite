5e882cb3b08a50e11664baa9d639d3eae2ab828f
datstarkey <jakestarkey@gmail.com>
2020-09-01 15:15:37 +0100

    Fix !dev

--- Startup.cs
+++ Startup.cs
@@ -84,7 +84,7 @@ namespace LadsOnTour
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            if (env.IsDevelopment())
+            if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles(new StaticFileOptions
                 {
